905efda386b6ee44cc52b2cec61e37d3c44fd204
kenpos <kenpos13@gmail.com>
2020-05-10 03:02:53 +0900

    Init

---
+++ BTP/Source/BTP/BTPGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BTPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BTP_API ABTPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
